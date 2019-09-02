9668008eab09a482d4cfca96aa8b12ffe017e2cb
github-patrixweb <54753946+github-patrixweb@users.noreply.github.com>
2019-09-01 19:52:22 -0700

    Add files via upload

---
+++ lab1.php
@@ -0,0 +1,62 @@
+<!doctype html>
+  <html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <title>Labwork 1: Hello World</title>
+       <style>
+       body {
+           width: 90%;
+            background-color: aliceblue;
+            margin: auto;
+            padding: 0.5em 0;
+            font-family: sans-serif;
+           font-size: 1.38em;
+           }
+
+        /* change link's color */
+           a {
+                    color: blue;
+                    }
+           a:visited {
+                        color: green;
+                    }
+            a:hover {
+                    color: pink;
+                    }
+            h1 {
+                    font-family: serif;
+                    padding: 0.5em 0em;
+                    }
+
+       </style>
+   </head>
+   <body>
+       <h1>About Me - Patrick Olesiak</h1>
+           <p>Former construction worker hit by a car at highway speed.  On July 29, 1991, I was riding an old family BMW motorcycle in the Palm Springs area.  A car pulled directly into my path; as a result, I t-boned the car, flew 100 feet through the air, and landed on my unprotected head.  I was in the hospital 16 months - the first three in a coma.  That is when I had brain surgery.  When out of the hospital, returning to drywall construction was not an option, so I enrolled in College of the Desert and studied computer science and information systems.  Worked full-time in the field seven years while attending college part-time.  Graduated with certificate in 1999, but had a second motorcycle accident five months later.  Broke my neck that time, and old injuries relapsed from my 1991 motorcycle accident.  I've been on disability since, but volunteered five years in a computer clinic while living in OC and five years at Aquarium of the Pacific while living in Long Beach.  But then I decided to return to college and earn a degree.  In 2017 I broke my hip in a bicycle accident, and while recovering from total hip replacement, changed my major to web development...  </p>
+            <p>Hobbies:</p>
+               <ul>
+                   <li>Biking</li>
+                   <li>Weight lifting</li>
+                   <li>Swimming</li>
+                   <li>Beach walks</li>
+                   <li>Building/maintaining bikes</li>
+                   <li>Building/programming/using computers</li>
+               </ul>
+            <a href="https://github.com/github-patrixweb" target="_blank" title="Patrick Olesiak's Profile on GitHub">Patrick Olesiak's Profile on GitHub</a>
+   <?php
+        /*
+        
+        lab1.php
+        Patrick Olesiak
+        August 31, 2019
+        
+        */
+        #output a paragragh in HTML
+        echo "<p>After my 1999 motorcycle accident, I did not ride for 16++ years.  However, in 2016 I decided to try an adult tricycle; that worked out for me due to recent balance issues.  Since that time, I assembled two of them.  And I often query Google about bike repair and maintenance questions.  One site that repeatedly came up with user threads on the subject is <a href='https://bikeforums.net' target='_blank'>Bike Forums</a>.  And for parts and accessories,I find <a href='https://www.amazon.com/s?k=cycling+accessories+for+bike&gclid=Cj0KCQjw2K3rBRDiARIsAOFSW_47iUiy1a4TvNUBSdYVsd_0-T19dpwpwuBK5Kl6iIJAPnTUmBPPZiUaAs-4EALw_wcB&hvadid=323347530889&hvdev=c&hvlocphy=9031542&hvnetw=g&hvpos=1t1&hvqmt=b&hvrand=5029445381829406785&hvtargid=kwd-505162740637&hydadcr=8590_9892164&tag=googhydr-20&ref=pd_sl_1270l2qu3v_b' target='_blank'>Amazon </a> is budget friendly.</p>
+        <p>This assignment was frustrating because it seemed like the cloud technologies involved were working against me.  But I learned about computers before the cloud was mainstream, so I will chalk this up as new experience and it'll get better for me with time.   
+        </p>";
+        
+    ?>
+
+      </body>
+  </html>
