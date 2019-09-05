6705bf205fbc1af056050f25e7a76d505e374ab0
Patrick Ole <54753946+github-patrixweb@users.noreply.github.com>
2019-09-05 11:39:12 -0700

    Update process_register.php

--- labwork/lab2/process_register.php
+++ labwork/lab2/process_register.php
@@ -13,13 +13,15 @@
     $confirm_password =$_POST['confirm_password'];
     
     if(!empty($first_name) && !empty($last_name) && !empty($email) && $password == $confirm_password){
-        echo '<h1>You logged in successfully!</h1>';
+        echo "<h1>Welcome to fantacy website,  $first_name $last_name</h1>";
+        echo "<h2>Your email was recorded as: $email</h2>";
     }
    else {
         //Error - output an error message
         echo '<h1>Sorry...try again!</h1>';
         echo '<a href="register.php">Go back</a>'; 
     }
-?>       
+?>
+       <p>Fantasy WebSite <a href="login.php" title="Fantasy WebSite login.">login</a>.</p>
       </body>
   </html>
