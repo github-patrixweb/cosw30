3d8ef6ed80defa3a11d8b46ac48aee66d228a314
Patrick Ole <ole@patrixweb.net>
2019-09-07 23:48:51 +0000

    update...

--- homework/week2/process_contact.php
+++ homework/week2/process_contact.php
@@ -5,10 +5,12 @@
 	<title>Form Feedback</title>
 </head>
 <body>
-<?php # Script 2.5 - handle_form.php #4
+<?php 
 // Print the submitted information:
 if ( !empty($_POST['first_name']) &&  !empty($_POST['last_name']) && !empty($_POST['age']) && !empty($_POST['email']) && !empty($_POST['gender']) ) {
-	echo "<p>Thank you, <strong>{$_POST['first_name'] . $_POST[last_name]}</strong>, your message was sent:</p>
+	echo $_POST['first_name'] . $_POST[last_name]
+	
+}
 <pre>{$_POST['first_name']}  . $_POST['last_name']</pre>
 <p>We will reply to you at <em>{$_POST['email']}</em>.</p>\n";
 } else { // Missing form value.
