128f00f123a2c96b96380092a42906dc35324c0e
Patrick Ole <ole@patrixweb.net>
2019-09-24 04:16:47 +0000

    lab4 upload

---
+++ labwork/lab4/lorum_ipsum_generator_process.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+
+Patrick Olesiak
+lorem_ipsum_generator.php
+Week 4 in-class labwork
+*/
+# create an array with 12 lorem epsum paragraphs
+$lorem_ipsum_12p = array (1 => 
+
+"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Congue quisque egestas diam in. Eget mauris pharetra et ultrices neque. Malesuada fames ac turpis egestas integer eget aliquet nibh praesent. Dolor sit amet consectetur adipiscing elit ut aliquam purus. Tristique senectus et netus et. Euismod elementum nisi quis eleifend quam. Velit aliquet sagittis id consectetur purus ut faucibus. Platea dictumst vestibulum rhoncus est pellentesque. Viverra nibh cras pulvinar mattis nunc sed. Diam donec adipiscing tristique risus nec. Amet aliquam id diam maecenas ultricies mi. Viverra justo nec ultrices dui sapien eget mi. Risus nec feugiat in fermentum posuere urna. Tincidunt augue interdum velit euismod. Mauris augue neque gravida in fermentum et. Aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Orci eu lobortis elementum nibh tellus molestie nunc. Volutpat diam ut venenatis tellus in metus.",
+
+"Nascetur ridiculus mus mauris vitae. Placerat in egestas erat imperdiet sed. Urna porttitor rhoncus dolor purus non. Fringilla phasellus faucibus scelerisque eleifend donec pretium. At tellus at urna condimentum mattis. Praesent semper feugiat nibh sed pulvinar proin gravida hendrerit lectus. Viverra tellus in hac habitasse platea. Eget nulla facilisi etiam dignissim. Fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate. Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere ac. Sem integer vitae justo eget magna fermentum iaculis eu non. Malesuada pellentesque elit eget gravida cum. Aliquet risus feugiat in ante metus dictum.",
+
+"Lacus laoreet non curabitur gravida arcu. Nibh cras pulvinar mattis nunc sed. Pharetra sit amet aliquam id. Scelerisque eu ultrices vitae auctor eu augue. Libero id faucibus nisl tincidunt eget nullam. Tincidunt lobortis feugiat vivamus at augue eget arcu. Sed sed risus pretium quam vulputate dignissim suspendisse in est. Risus nullam eget felis eget. Non blandit massa enim nec dui. Ornare aenean euismod elementum nisi quis eleifend quam adipiscing. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Vulputate sapien nec sagittis aliquam malesuada bibendum. Tellus molestie nunc non blandit massa. Ultricies leo integer malesuada nunc vel risus commodo viverra. Pellentesque nec nam aliquam sem et tortor consequat id. Vitae et leo duis ut diam. Mauris sit amet massa vitae. Vel facilisis volutpat est velit egestas dui id.",
+
+"Eget egestas purus viverra accumsan in. Tincidunt tortor aliquam nulla facilisi cras fermentum odio. Vestibulum lectus mauris ultrices eros in cursus. Tellus orci ac auctor augue. Cursus mattis molestie a iaculis at erat pellentesque adipiscing. Feugiat pretium nibh ipsum consequat nisl vel pretium. Cursus metus aliquam eleifend mi in nulla. In ante metus dictum at tempor commodo ullamcorper a. Luctus accumsan tortor posuere ac ut consequat semper viverra. Blandit massa enim nec dui nunc mattis enim ut. Purus sit amet volutpat consequat mauris nunc. Faucibus vitae aliquet nec ullamcorper sit amet risus nullam. Morbi tristique senectus et netus et malesuada fames ac turpis. Curabitur vitae nunc sed velit dignissim. In hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit.",
+
+"Lacus laoreet non curabitur gravida. Ultrices gravida dictum fusce ut placerat orci nulla pellentesque dignissim. Ac tortor vitae purus faucibus ornare. In metus vulputate eu scelerisque felis imperdiet proin fermentum. Urna porttitor rhoncus dolor purus non enim praesent elementum facilisis. Mauris commodo quis imperdiet massa tincidunt. Placerat duis ultricies lacus sed turpis. Varius quam quisque id diam vel quam. Accumsan tortor posuere ac ut. Ornare quam viverra orci sagittis eu volutpat odio facilisis mauris.",
+
+"Pellentesque pulvinar pellentesque habitant morbi tristique senectus et. Ac odio tempor orci dapibus ultrices in iaculis nunc. Sit amet volutpat consequat mauris nunc congue nisi. Cursus turpis massa tincidunt dui ut ornare lectus sit amet. Pellentesque habitant morbi tristique senectus et netus. Viverra aliquet eget sit amet tellus cras. Vulputate dignissim suspendisse in est ante in nibh. Dolor sit amet consectetur adipiscing elit ut. Mattis nunc sed blandit libero volutpat sed cras ornare arcu. Viverra orci sagittis eu volutpat odio.",
+
+"Turpis nunc eget lorem dolor sed viverra ipsum nunc. Amet nisl purus in mollis nunc sed id semper risus. Laoreet sit amet cursus sit amet dictum sit amet justo. A arcu cursus vitae congue mauris rhoncus aenean vel. Morbi blandit cursus risus at. Dignissim cras tincidunt lobortis feugiat vivamus at augue eget arcu. Dui id ornare arcu odio ut sem nulla pharetra diam. Maecenas pharetra convallis posuere morbi leo urna. Est pellentesque elit ullamcorper dignissim cras tincidunt. Consectetur adipiscing elit ut aliquam purus sit amet luctus venenatis. Viverra nam libero justo laoreet sit amet cursus sit. Diam quam nulla porttitor massa id neque aliquam vestibulum morbi. Malesuada pellentesque elit eget gravida. Iaculis at erat pellentesque adipiscing. Volutpat blandit aliquam etiam erat. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Morbi tristique senectus et netus et malesuada. Accumsan lacus vel facilisis volutpat est velit egestas.",
+
+"Interdum velit laoreet id donec ultrices tincidunt arcu non. Risus in hendrerit gravida rutrum quisque non. Tortor dignissim convallis aenean et tortor at risus viverra. Scelerisque varius morbi enim nunc. Elit ut aliquam purus sit. Augue ut lectus arcu bibendum at. Aliquam sem et tortor consequat id porta nibh. Blandit turpis cursus in hac habitasse platea dictumst. Eleifend mi in nulla posuere sollicitudin aliquam. Nec dui nunc mattis enim ut tellus. Egestas maecenas pharetra convallis posuere morbi leo urna molestie. Rutrum tellus pellentesque eu tincidunt. Massa eget egestas purus viverra. Senectus et netus et malesuada.",
+
+"Augue mauris augue neque gravida. In ornare quam viverra orci sagittis eu volutpat odio facilisis. Imperdiet nulla malesuada pellentesque elit eget gravida cum. Integer feugiat scelerisque varius morbi enim nunc. Nascetur ridiculus mus mauris vitae. Semper auctor neque vitae tempus quam pellentesque nec nam aliquam. Nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque. Lobortis mattis aliquam faucibus purus in massa tempor. Vehicula ipsum a arcu cursus vitae. Ac turpis egestas sed tempus urna et pharetra pharetra massa. Ante in nibh mauris cursus mattis molestie a iaculis at. Cras sed felis eget velit. A iaculis at erat pellentesque adipiscing commodo elit at. Duis ut diam quam nulla porttitor massa id neque aliquam. Sit amet luctus venenatis lectus magna fringilla urna porttitor. Nibh praesent tristique magna sit amet purus gravida quis. Amet nulla facilisi morbi tempus iaculis urna id volutpat lacus. Consectetur purus ut faucibus pulvinar elementum integer enim. Suspendisse potenti nullam ac tortor. Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet risus.",
+
+"Lectus proin nibh nisl condimentum id. Sagittis orci a scelerisque purus semper eget duis at tellus. Quis risus sed vulputate odio ut enim blandit. Iaculis nunc sed augue lacus viverra vitae congue eu. Ipsum nunc aliquet bibendum enim. Sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus. Tempus egestas sed sed risus pretium quam vulputate dignissim suspendisse. Laoreet non curabitur gravida arcu ac tortor dignissim. Congue quisque egestas diam in arcu. Orci ac auctor augue mauris augue neque gravida in fermentum. Eget sit amet tellus cras adipiscing enim eu turpis. Nunc id cursus metus aliquam. Mattis enim ut tellus elementum sagittis vitae et leo duis. Nunc vel risus commodo viverra maecenas accumsan lacus vel. Ut enim blandit volutpat maecenas volutpat blandit aliquam etiam erat.",
+
+"Sagittis orci a scelerisque purus semper eget duis at tellus. Proin nibh nisl condimentum id. Vel quam elementum pulvinar etiam non quam lacus suspendisse faucibus. Diam maecenas ultricies mi eget mauris pharetra et ultrices. Ac tortor dignissim convallis aenean et. Eget aliquet nibh praesent tristique magna. Risus commodo viverra maecenas accumsan lacus vel facilisis volutpat. Eget lorem dolor sed viverra ipsum nunc aliquet bibendum. Malesuada pellentesque elit eget gravida cum sociis natoque penatibus. In nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque. Dolor morbi non arcu risus quis. Proin fermentum leo vel orci porta. Pharetra et ultrices neque ornare aenean.",
+
+"Tempor id eu nisl nunc mi ipsum. Nunc aliquet bibendum enim facilisis gravida. Risus nullam eget felis eget nunc lobortis. Arcu vitae elementum curabitur vitae nunc. Amet volutpat consequat mauris nunc congue nisi. Odio ut enim blandit volutpat maecenas volutpat blandit. Adipiscing commodo elit at imperdiet dui accumsan sit. Morbi non arcu risus quis varius quam. Luctus accumsan tortor posuere ac ut consequat semper. Nulla aliquet porttitor lacus luctus accumsan tortor posuere ac ut. Rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Eget dolor morbi non arcu risus quis varius quam. Eu facilisis sed odio morbi quis commodo odio aenean sed. Pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. Ultrices sagittis orci a scelerisque purus. Aenean euismod elementum nisi quis eleifend quam. Volutpat blandit aliquam etiam erat. In metus vulputate eu scelerisque felis imperdiet proin fermentum. Sit amet est placerat in egestas erat imperdiet sed. Feugiat in ante metus dictum at tempor.",
+);
+$lip = $_POST["lip"];
+?>
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+       <meta charset="utf-8">
+       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="css/lig_style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+       <title>Lorem Ipsum Generator Response</title>
+    </head>
+    <body>
+       <div id="lorem-ipsum">
+        <img src="https://loremflickr.com/800/222" width="800" height="222"/>
+        <h1>You selected <?php echo $lip ?> paragraphs of Lorum Ipsum text.</h1>
+           <h2>It is ready to be copied.</h2>
+         <?php
+            $li_copy = "<p id='liTxt'>";
+          # output selected lorem ipsum paragraphs
+           for ($i = 1; $i <= $lip; $i++){
+               echo "<p>$lorem_ipsum_12p[$i]</p>\n";
+               
+           }
+            
+        ?>   
+           
+        </div>   
+    </body>
+</html>      
