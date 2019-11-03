92bd2a94deacd1e9a723573e1471c243fb992166
Patrick Ole <lbcc@patrixweb.net>
2019-11-03 06:59:48 +0000

    #10 progress

--- homework/week-10/crud.php
+++ homework/week-10/crud.php
@@ -1,64 +1,124 @@
+<!doctype html>
+<html>
+<head>
+   <style>
+       .error {
+           color: red;
+       }
+       .success {
+           color: green;
+       }
+    </style>
+    <title>MySQL CRUD</title>
+</head>
+<body>
+
 <?php
 // Add the database connection
-include 'database.php';
-/*
+include 'includes/database.php';
+//set sticky form variables to empty strings in casse script doen not change them.
+$first_name = "";
+$last_name = "";
+$password = "";
+$email = "";
+
+    /*
 *   CHECK IF THE FORM HAS BEEN SUBMITTED AND INSERT
 *   NEW USER INTO THE DATABASE
 */
+
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
-$first_name = $_POST['first_name'];
-$last_name = $_POST['last_name'];
-$email = $_POST['email'];
-$password = $_POST['password'],
+    $valid_input = 0;
+    // if any of the 4 form variable are empty, display error
+    if (!empty($_POST['first_name'])){
+        $first_name = $_POST['first_name'];
+        ++$valid_input;
+    }
+    else {
+        echo "<h2 class='error'>First name is required.</h2>";
+    }
+    if (!empty($_POST['last_name'])){
+        $last_name = $_POST['last_name'];
+        ++$valid_input;
+    }
+    else {
+        echo "<h2 class='error'>Last name is required.</h2>";
+    }
+    if (!empty($_POST['email'])){
+        $email = $_POST['email'];
+        ++$valid_input;
+    }
+    else {
+        echo "<h2 class='error'>Email is required.</h2>";
+    }
+    if (!empty($_POST['password'])){
+        if ($_POST['password'] != $_POST['confirm_password']){
+            echo "<h2 class='error'>Your passwords did not match.</h2>";
+        }
+        else {
+            ++$valid_input;
+        }
+        $password = $_POST['password'];
+
+    }
+    else {
+        echo "<h2 class='error'>Password is required.</h2>";
+    }
+    // if all input not empty and passwords match, input in database
+    if ($valid_input == 4) {
+        $query = "INSERT INTO USER_OLESIAK (first_name, last_name, email, password)
+                    VALUES ('$first_name', '$last_name', '$email', '$password')";
+        if ($connection->query($query) === true) {
+            echo "<h2 class='success'>Record added succesfully</h2>";
+        }
+        else {
+             echo "<h2 class='error'>Error:  $sql <br> $connection->error</h2>";
+        }
+    }
+
 }
-echo "$first_name $last_name, $email, $password";
+
+
+?>
+
+    <h1>Create a New User</h1>
+    <form action="crud.php" method="POST">
+        <label for="first_name">First Name</label>
+        <input type="text" id="first_name" name="first_name" value="<?php echo $first_name ?>"><br>
+
+        <label for="last_name">Last Name</label>
+        <input type="text" id="last_name" name="last_name" value="<?php echo $last_name ?>"><br>
+
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" value="<?php echo $email ?>"><br>
+
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password" value="<?php echo $password ?>"><br>
+
+        <label for="confirm_password">Retype Password</label>
+        <input type="password" id="confirm_password" name="confirm_password"><br>
+
+        <!--Add a second password input so the user has to retype their password -->
+
+        <button>Register</button>
+
+    </form>
+
+    <h2>Output a List of Users</h2>
+<?php
 
 /*
 *   QUERY THE DATABASE AND STORE ALL USERS INTO A VARIABLE
 */
 // Create your query
-$query = 'SELECT * FROM USER';
+$query = 'SELECT * FROM USER_OLESIAK';
 
 // Run your query
 $result = mysqli_query($connection, $query);
 
 // Check if the database returned anything
 if($result) {
-    while($row = mysqli_fetch_array($result)){
-        // Output the results
-    }
-} else {
-    // Output an error
-}
-?>
-
-<!doctype html>
-<html>
-<head>
-    <title>My First CRUD</title>
-</head>
-<body>
-    <h1>Create a New User</h1>
-    <form action="crud.php" method="POST">
-        <label for="first_name">First Name</label>
-        <input type="text" id="first_name" name="first_name"><br>
-
-        <label for="last_name">Last Name</label>
-        <input type="text" id="last_name" name="last_name"><br>
-
-        <label for="email">Email</label>
-        <input type="email" id="email" name="email"><br>
-
-        <label for="password">Password</label>
-        <input type="password" id="password" name="password"><br>
-
-        <!--Add a second password input so the user has to retype their password -->
-
-        <button>Register</button>
-    </form>
-
-    <h2>Output a List of Users</h2>
-    <table>
+    echo "   <table border='1'>
         <thead>
             <tr>
                 <th>First Name</th>
@@ -67,14 +127,29 @@ if($result) {
                 <th>Password</th>
             </tr>
         </thead>
-        <tbody>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-        </tbody>
-    </table>
-</body>
+        <tbody>";
+   // while($row = mysqli_fetch_array($result)){
+        // Output the results
+
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr>
+                <td>" . $row['first_name'] . "</td>\n
+                <td>" . $row['last_name'] . "</td>\n
+                <td>" . $row['email'] . "</td>\n
+                <td>" . $row['password'] . "</td>\n
+            </tr>";
+
+    }
+    echo "</tbody>
+    </table>";
+
+    }
+ else {
+    // Output an error
+    die("<h2 class='error'>Connection failed:  $conn->connect_error</h2>");
+}
+?>
+
+        </body>
 </html>
