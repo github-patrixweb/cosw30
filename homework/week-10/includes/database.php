20d0dd852bef6e9e8ba604b38140ab472129ed19
Patrick Ole <lbcc@patrixweb.net>
2019-11-03 07:18:31 +0000

    db update II

--- homework/week-10/includes/database.php
+++ homework/week-10/includes/database.php
@@ -1,16 +1,10 @@
 <?php
 // Create environment variables
-/*
+
 $db_hostname   = getenv('DB_HOSTNAME');
 $db_user   = getenv('DB_USER');
 $db_pass   = getenv('DB_PASS');
 $db_name     = getenv('DB_NAME');
-*/
-/* these are for local use*/
-$db_hostname   = 'denal.sgedu.site';
-$db_user   = 'denal305';
-$db_pass   = 'Lb2019!';
-$db_name     = 'denal305_UI';
 
 // Establish the connection to the database
 $connection = mysqli_connect($db_hostname, $db_user, $db_pass, $db_name);
