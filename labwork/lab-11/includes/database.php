6e171cfd6ed757b364a1ee924262b5078f93d38b
Patrick Ole <54753946+github-patrixweb@users.noreply.github.com>
2019-11-22 17:59:27 -0800

    db vars...

--- labwork/lab-11/includes/database.php
+++ labwork/lab-11/includes/database.php
@@ -1,7 +1,7 @@
 <?php
 $db_hostname   = getenv('DB_HOSTNAME');
-$db_username   = getenv('DB_USER');
-$db_password   = getenv('DB_PASS');
-$db_dbname     = getenv('DB_NAME');
+$db_user   = getenv('DB_USER');
+$db_pass   = getenv('DB_PASS');
+$db_name     = getenv('DB_NAME');
 $connection = mysqli_connect($db_hostname, $db_user, $db_pass, $db_name);
 ?>
