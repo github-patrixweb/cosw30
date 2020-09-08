e23a4b64d28e6a5ebde4edec9e6ece41a1988c72
Patrick Ole <54753946+github-patrixweb@users.noreply.github.com>
2020-09-08 06:45:31 -0700

    Delete frontEndWebDevelopment.php

--- frontEndWebDevelopment.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$headTitle = "Front-end Web Development";
-$title = "Front-end";
-include "includes/po_portfolio_header.html"; 
-include "includes/po_frontEndWebDev.html"; 
-include "includes/po_portfolio_footer.html"; ?>
