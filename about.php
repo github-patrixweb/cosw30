07e9c6bc4eb4e7847f7a4b4b0f800c13cf3d5bc0
Patrick Ole <54753946+github-patrixweb@users.noreply.github.com>
2020-09-08 06:55:41 -0700

    Delete about.php

--- about.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$headTitle = "About Patrick Ole's Web Development Journey";
-$title = "About";
-include "includes/po_portfolio_header.html"; 
-include "includes/po_portfolio_about.html"; 
-include "includes/po_portfolio_footer.html"; ?>
