9788e241c494fa1907809517e233f399e1e346af
Patrick Ole <54753946+github-patrixweb@users.noreply.github.com>
2020-09-08 06:57:09 -0700

    Delete classes.php

--- classes.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$headTitle = "Patrick Ole's College Classes";
-$title = "Classes";
-include "includes/po_portfolio_header.html"; 
-include "includes/po_classes.html";
-include "includes/po_portfolio_footer.html"; ?>
