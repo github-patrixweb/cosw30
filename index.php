e8944a6523ddc3146261d4d637758f8a1fda8830
Patrick Ole <54753946+github-patrixweb@users.noreply.github.com>
2020-09-08 06:53:28 -0700

    Redirect to latest server...

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
-<?php 
-$headTitle = "Patrick Ole's Web Development Portfolio";
-$title = "Home";
-include "includes/po_portfolio_header.html";
-include "includes/po_home.html";
-include "includes/po_portfolio_footer.html"; ?>
+<?php
+header("Location: https://patrick-olesiak.amazingly-wonderful.com"); /* Redirect browser */
+
+/* Make sure that code below does not get executed when we redirect. 
+2020.9.8 redirect to new server
+*/
+exit;
+?>
