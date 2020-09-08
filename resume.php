95f25ac2584bc92aa97be4fb89a82e616c174256
Patrick Ole <54753946+github-patrixweb@users.noreply.github.com>
2020-09-08 06:47:25 -0700

    Delete resume.php

--- resume.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$headTitle = "Patrick Ole's Resume";
-$title = "Resume";
-include "includes/po_portfolio_header.html"; ?>
-    <main>   
-        <?php include "includes/po_resume.html"; ?>   
-    </main>
-    <?php include "includes/po_portfolio_footer.html"; ?>
