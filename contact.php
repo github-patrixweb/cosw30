ef355757a6037cab1a7cf58992a58863543a61de
Patrick Ole <54753946+github-patrixweb@users.noreply.github.com>
2020-09-08 06:57:43 -0700

    Delete contact.php

--- contact.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$headTitle = "Contact Patrick Ole and inquire about web publishing services";
-$title = "Contact";
-include "includes/po_portfolio_header.html";
-include "includes/po_portfolio_contact.html";
-include "includes/po_portfolio_footer.html"; ?>
